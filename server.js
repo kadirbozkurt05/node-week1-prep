dd681a62c91716b40dd5131d9a837f37b353773d
Kadir Bozkurt <kadirbozkurt05@yandex.com>
2023-10-27 17:37:47 +0200

    delete comments

--- server.js
+++ server.js
@@ -1,51 +1,44 @@
-/**
- * Exercise 3: Create an HTTP web server
- */
+const http = require("http");
+const fs = require("fs");
 
-const http = require('http');
-const fs = require('fs');
-
-//create a server
 let server = http.createServer(function (req, res) {
-  // YOUR CODE GOES IN HERE
-
-  if(req.url==='/'){
-  fs.readFile('index.html',(error, data)=>{
-	if(error){
-		res.setHeader('content-type','text/plain');
-		res.write('Error!');
-		res.end();
-	}else{
-		res.setHeader('content-type','text/html')
-		res.write(data);
-		res.end();
-	}
-  })
-  }  else if(req.url==='/index.js'){
-	fs.readFile('index.js',(error, data)=>{
-		if(error){
-			res.setHeader('content-type','text/plain');
-			res.write('Error!');
-			res.end();
-		}else{
-			res.setHeader('content-type','application/javascript')
-			res.write(data);
-			res.end();
-		}
-	  })
-}else if(req.url==='/style.css'){
-	fs.readFile('style.css',(error, data)=>{
-		if(error){
-			res.setHeader('content-type','text/plain');
-			res.write('Error!');
-			res.end();
-		}else{
-			res.setHeader('content-type','text/css')
-			res.write(data);
-			res.end();
-		}
-	  })
-}
+  if (req.url === "/") {
+    fs.readFile("index.html", (error, data) => {
+      if (error) {
+        res.setHeader("content-type", "text/plain");
+        res.write("Error!");
+        res.end();
+      } else {
+        res.setHeader("content-type", "text/html");
+        res.write(data);
+        res.end();
+      }
+    });
+  } else if (req.url === "/index.js") {
+    fs.readFile("index.js", (error, data) => {
+      if (error) {
+        res.setHeader("content-type", "text/plain");
+        res.write("Error!");
+        res.end();
+      } else {
+        res.setHeader("content-type", "application/javascript");
+        res.write(data);
+        res.end();
+      }
+    });
+  } else if (req.url === "/style.css") {
+    fs.readFile("style.css", (error, data) => {
+      if (error) {
+        res.setHeader("content-type", "text/plain");
+        res.write("Error!");
+        res.end();
+      } else {
+        res.setHeader("content-type", "text/css");
+        res.write(data);
+        res.end();
+      }
+    });
+  }
 });
 
-server.listen(3000); // The server starts to listen on port 3000
+server.listen(3000);
